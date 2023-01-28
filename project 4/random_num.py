0e1985e9664099b7917e910a7f41d189b0d10fe3
Cuauhtli <cuau_daali@hotmail.com>
2023-01-27 19:29:28 -0600

    Porject 4 complete

---
+++ project 4/random_num.py
@@ -0,0 +1,28 @@
+from random import randint
+
+numRandom = randint(1, 100)
+
+name = input('¿Cual es tu nombre?: ')
+
+print(f"Bueno, {name}, he pensado un número entre 1 y 100, y tienes solo ocho intentos para adivinar cuál crees que es el número")
+
+attempts = 8
+
+while attempts >= 0:
+    if attempts > 0:
+        choiceNum = input()
+        if int(choiceNum) < 1 or int(choiceNum) > 100:
+            print("Ha elegido un número que no está permitido")
+        elif int(choiceNum) < numRandom:
+            attempts -= 1
+            print(f"Ha elegido un número menor al número secreto, le quedan {attempts} intentos")   
+        elif int(choiceNum) > numRandom:
+            attempts -= 1
+            print(f"Ha elegido un número mayor al número secreto, le quedan {attempts} intentos")
+        elif int(choiceNum) == numRandom:
+            attempts -= 1
+            print(f"Ha eligido el numero secreto, le tomo {8 - attempts}")
+            break
+    elif attempts == 0:
+        print("Te haz quedado sin intentos")
+        break
