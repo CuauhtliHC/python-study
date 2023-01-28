a41e22d8f0f0a92f903a64268bbf19d46b2f806a
Cuauhtli <cuau_daali@hotmail.com>
2023-01-28 15:50:22 -0600

    Project 5 complete

--- project 5/hanged_game.py
+++ project 5/hanged_game.py
@@ -1,16 +1,77 @@
 from random import choice
 
-def randomWord():
-    wordList = ["hola", "mundo", "juego", "zoologico"]
-    return list(choice(wordList))
+palabras = ["hola", "mundo", "juego", "zoologico"]
+letras_correctas = []
+letras_incorrectas = []
+intentos = 6
+aciertos = 0
+juego_terminado = False
 
-def letterUser():
-    print(randomWord())
-    valid = True
-    while valid:
-        letter = input("Ingresa una letra: ")
-        if letter.isalpha():
-            valid = False
 
-def checkLetters(letter, letterList):
-    return any(letter in word for word in letterList)
+def elegir_palbra(lista_palabras):
+    palabra_elegida = choice(lista_palabras)
+    letras_unicas = len(set(palabra_elegida))
+
+    return palabra_elegida, letras_unicas
+
+def pedir_letra():
+    letra_elegida = ''
+    es_valida = False
+    while not es_valida:
+        letra_elegida = input("Ingresa una letra: ").lower()
+        if letra_elegida.isalpha() and len(letra_elegida) == 1:
+            es_valida = True
+        else:
+            print("No has elegido una letra correcta")
+    return letra_elegida
+
+def mostrar_nuevo_tablero(palabra_elegida):
+    lista_oculta = []
+    for l in palabra_elegida:
+        if l in letras_correctas:
+            lista_oculta.append(l)
+        else:
+            lista_oculta.append('-')
+    print(' '.join(lista_oculta))
+
+def chequear_letra(letra_elegida, palabra_oculta, vidas, coincidencias):
+    fin = False
+    if letra_elegida in palabra_oculta and letra_elegida not in letras_correctas:
+        letras_correctas.append(letra_elegida)
+        coincidencias += 1
+    elif letra_elegida in palabra_oculta and letra_elegida in letras_correctas:
+        print("Ya has encontrado esa letra. Intenta con otra diferente")
+    else:
+        letras_incorrectas.append(letra_elegida)
+        vidas -= 1
+    
+    if vidas == 0:
+        fin = perder()
+    elif coincidencias == letras_unicas:
+        fin = ganar(palabra_oculta)
+
+    return vidas, fin, coincidencias
+
+def perder():
+    print("Game Over")
+    print("La palabra oculta era " + palabra) 
+
+    return True
+
+def ganar(palabra_descubierta):
+    mostrar_nuevo_tablero(palabra_descubierta)
+    print("Felicitaciones, has encontrado la palabra!!!")
+    return True
+
+palabra, letras_unicas = elegir_palbra(palabras)
+
+while not juego_terminado:
+    print('\n' + '*' * 20 + '\n')
+    mostrar_nuevo_tablero(palabra)
+    print('\n')
+    print("Letras incorrectas: " + "-".join(letras_incorrectas))
+    print(f"Vidas: {intentos}")
+    print('\n' + '*' * 20 + "\n")
+    letter = pedir_letra()
+    intentos, terminado, aciertos = chequear_letra(letter, palabra, intentos, aciertos)
+    juego_terminado = terminado
